304a66c8e45d856849f0dab719ad831ffa0b44b1
Diana Alfaro <dicaalba@gmail.com>
2026-07-17 10:35:40 -0500

    fix: actualizar API_BASE_URL a API Gateway (Function URL bloqueada por SCP)

--- public/js/config.js
+++ public/js/config.js
@@ -3,6 +3,6 @@
  * Actualiza API_BASE_URL con la URL de tu Lambda Function URL
  */
 const CONFIG = {
-  API_BASE_URL: 'https://lhbgec6hhqgjnbcwewmrs2winu0lyavd.lambda-url.us-east-1.on.aws',
+  API_BASE_URL: 'https://hkhkh8v50h.execute-api.us-east-1.amazonaws.com',
   MEETUP_URL: 'https://www.meetup.com/aws-girls-peru/'
 };
